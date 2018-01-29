f68e876fadabb6d7e665c47756f3982040b2e5d7
Steven Orvell <sorvell@google.com>
2018-01-29 10:45:35 -0800

    Adds didRender and render helpers
    * Adds `didRender` which is called after render.
    * Always calls `render` from `propertiesChanged`
    * adds `renderAttributes` helper intended to help render attributes to host element
    * adds `classString` helper intended to help compose properties to a class bindings; includes properties in the given object if the values are truthy {foo: true, bar: 1} => 'foo bar'
    * adds `styleString` helper intended to help compose properties to style bindings; flattens properties into `;` separated values.

--- src/@polymer/lit-element.ts
+++ src/@polymer/lit-element.ts
@@ -12,53 +12,130 @@
  * http://polymer.github.io/PATENTS.txt
  */
 import { PropertiesMixin } from '../../@polymer/polymer/lib/mixins/properties-mixin.js';
+import { camelToDashCase } from '../../@polymer/polymer/lib/utils/case-map.js';
 import { TemplateResult } from '../../lit-html/lit-html.js';
 import { render } from '../../lit-html/lib/lit-extended.js';
 
 export { html } from '../../lit-html/lit-html.js';
+
+/**
+ * Renders attributes to the given element based on the `attrInfo` object where
+ * boolean values are added/removed as attributes.
+ * @param {*} element Element on which to set attributes.
+ * @param {*} attrInfo Object describing attributes.
+ */
+export function renderAttributes(element: HTMLElement, attrInfo: any) {
+  for (const a in attrInfo) {
+    const v = attrInfo[a] === true ? '' : attrInfo[a];
+    if (v || v === '' || v === 0)  {
+      if (element.getAttribute(a) !== v) {
+        element.setAttribute(a, v);
+      }
+    } else if (element.hasAttribute(a)) {
+      element.removeAttribute(a);
+    }
+  }
+}
+
+/**
+ * Returns a string of css class names formed by taking the properties
+ * in the `classInfo` object and appending the property name to the string of
+ * class names if the property value is truthy.
+ * @param {*} classInfo
+ */
+export function classString(classInfo: any) {
+  const o = [];
+  for (const name in classInfo) {
+    const v = classInfo[name];
+    if (v) {
+      o.push(name);
+    }
+  }
+  return o.join(' ');
+}
+
+/**
+ * Returns a css style string formed by taking the properties in the `styleInfo`
+ * object and appending the property name (dash-cased) colon the
+ * property value. Properties are separated by a semi-colon.
+ * @param {*} styleInfo
+ */
+export function styleString(styleInfo: any) {
+  const o = [];
+  for (const name in styleInfo) {
+    const v = styleInfo[name];
+    if (v || v === 0) {
+      o.push(`${camelToDashCase(name)}: ${v}`);
+    }
+  }
+  return o.join('; ');
+}
+
 export class LitElement extends PropertiesMixin(HTMLElement) {
 
-  _nextRendered: Promise<any>|null = null;
-  _nextRenderedResolver: Function|null = null;
+  private __nextRendered: Promise<any>|null = null;
+  private __nextRenderedResolver: Function|null = null;
 
-  ready() {
+  protected ready() {
     this.attachShadow({mode: 'open'});
     super.ready();
   }
 
-  _flushProperties() {
-    super._flushProperties();
-    // TODO(sorvell): propertiesChanged should have `_getData`
-    const result = this.render(this.__data);
+  protected _shouldPropertiesChange() { return true; }
+
+  // TODO(sorvell): push to PropertiesChanged
+  protected _flushProperties() {
+    if (this._shouldPropertiesChange()) {
+      let changedProps = this.__dataPending;
+      this.__dataPending = null;
+      this._propertiesChanged(this.__data, changedProps, this.__dataOld);
+    }
+  }
+
+  protected _propertiesChanged(props: any, ...args) {
+    super._propertiesChanged(props, ...args);
+    const result = this.render(props);
     if (result) {
       render(result, this.shadowRoot!);
     }
-    if (this._nextRenderedResolver) {
-      this._nextRenderedResolver();
-      this._nextRenderedResolver = null;
-      this._nextRendered = null;
+    this.didRender();
+    if (this.__nextRenderedResolver) {
+      this.__nextRenderedResolver();
+      this.__nextRenderedResolver = null;
+      this.__nextRendered = null;
     }
   }
 
   /**
    * Return a template result to render using lit-html.
    */
-  render(_props: object): TemplateResult {
+  protected render(_props: object): TemplateResult {
     throw new Error('render() not implemented');
   }
 
+  /**
+   * Method called after rendering; can be used to directly access element DOM.
+   */
+  protected didRender() {}
+
+  /**
+   * Provokes the element to asynchronously re-render.
+   */
   invalidate() {
     this._invalidateProperties();
   }
 
+  /**
+   * Returns a promise which resolves after the element next renders.
+   */
   get nextRendered() {
-    if (!this._nextRendered) {
+    if (!this.__nextRendered) {
       // TODO(sorvell): handle rejected render.
-      this._nextRendered = new Promise((resolve) => {
-        this._nextRenderedResolver = resolve;
+      this.__nextRendered = new Promise((resolve) => {
+        this.__nextRenderedResolver = resolve;
       });
     }
-    return this._nextRendered;
+    return this.__nextRendered;
   }
 
 }
