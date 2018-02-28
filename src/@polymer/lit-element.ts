9c5b46562f28115538efda673465421fd0b19cea
Steven Orvell <sorvell@google.com>
2018-02-28 14:08:27 -0800

    Use console.trace

--- src/@polymer/lit-element.ts
+++ src/@polymer/lit-element.ts
@@ -130,7 +130,7 @@ export class LitElement extends PropertiesMixin(HTMLElement) {
   _shouldPropertyChange(property: String, value: any, old: any) {
     const change = super._shouldPropertyChange(property, value, old);
     if (change && this.__isChanging) {
-      console.warn(`Setting propertyes in response to other properties changing considered harmful. Setting '${name}' from '${this._getProperty(name)}' to '${value}'. See stack trace for more info.`);
+      console.trace(`Setting propertyes in response to other properties changing considered harmful. Setting '${name}' from '${this._getProperty(name)}' to '${value}'.`);
     }
     return change;
   }
