2dd9ccab5148860686c9fe8b9fb494c218171a8a
43081j <43081j@users.noreply.github.com>
2018-10-17 22:37:12 +0100

    use async/await in project loader

--- docs/src/project-loader.js
+++ docs/src/project-loader.js
@@ -16,43 +16,47 @@ export class ProjectLoader extends LitElement {
     this.project = {};
     this.loadProject();
   }
-  
-  loadProject() {
+
+  async loadProject() {
     if (this.folder == undefined) {
-      console.log("Missing folder property.");
+      console.log('Missing folder property.');
       return;
-    } else console.log('loading', this.folder);
-    fetch(`${this.folder}/manifest.json`)
-    .then(response => { return response.json() })
-    .then(manifest => {      
-      return this.getFiles(manifest).then(filesArray => {
-        var filesObject = filesArray.reduce((prev, curr) => {
-          return Object.assign({}, prev, curr);
-        });
-        this.project = Object.assign({}, manifest, { files: filesObject });
-        var projectLoadedEvent = new CustomEvent('project-loaded', { 
-          detail: { project: this.project },
-          bubbles: true
-        });
-        return projectLoadedEvent;
-      }).then((projectLoadedEvent) => { 
-        this.dispatchEvent(projectLoadedEvent); 
-      }).catch((error) => { console.log(error) });
-    }).catch((error) => { console.log(error) });
+    } else {
+      console.log('loading', this.folder);
+    }
+
+    try {
+      const response = await fetch(`${this.folder}/manifest.json`);
+      const manifest = await response.json();
+      const filesArray = await this.getFiles(manifest);
+      const filesObject = filesArray.reduce((prev, curr) => {
+        return Object.assign({}, prev, curr);
+      });
+
+      this.project = Object.assign({}, manifest, {files: filesObject});
+      const projectLoadedEvent = new CustomEvent('project-loaded', {
+        detail: {project: this.project},
+        bubbles: true
+      });
+      this.dispatchEvent(projectLoadedEvent);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  getFiles(manifest) {
-    return Promise.all(manifest.files.map(filename => { return (
-      fetch(`${this.folder}/${filename}`)
-      .then(result => {
-        return result.text().then(r => { 
-          return { [filename]: this.uglyHack(r) }
-        }).catch((error) => {console.log(error)});
-      })
-    )})).catch((error) => {console.log(error)});
+
+  async getFiles(manifest) {
+    return Promise.all(manifest.files.map(async (filename) => {
+      const response = await fetch(`${this.folder}/${filename}`);
+      const text = await response.text();
+      return {
+        [filename]: this.uglyHack(text)
+      };
+    }));
   }
+
   uglyHack(r) {
-    var regex = /\.\.\//g;
-    return (r.replace("../node_modules/", "").replace(regex, ""));
+    const regex = /\.\.\//g;
+    return (r.replace('../node_modules/', '').replace(regex, ''));
   }
 }
 customElements.define('project-loader', ProjectLoader);
