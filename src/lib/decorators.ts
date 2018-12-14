47de92b150e9dedd5eb285e7760ef75f9c9725db
Steven Orvell <sorvell@google.com>
2018-12-13 17:37:19 -0800

    Update to use `AddEventListenerOptions`

--- src/lib/decorators.ts
+++ src/lib/decorators.ts
@@ -90,16 +90,6 @@ function _query<T>(queryFn: (target: NodeSelector, selector: string) => T) {
   };
 }
 
-// TODO(sorvell): augment TypeScript's EventListenerOptions which does
-// not include these options despite
-// https://github.com/Microsoft/TypeScript/issues/9548
-declare global {
-  interface EventListenerOptions {
-    passive?: boolean;
-    once?: boolean;
-  }
-}
-
 /**
  * Adds event listener options to a method used as an event listener in a
  * lit-html template.
@@ -126,7 +116,7 @@ declare global {
  *       }
  *     }
  */
-export const eventOptions = (options: EventListenerOptions) =>
+export const eventOptions = (options: AddEventListenerOptions) =>
     (proto: any, name: string) => {
       // This comment is here to fix a disagreement between formatter and linter
       Object.assign(proto[name], options);
