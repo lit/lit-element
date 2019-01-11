0797a6b8c060e1153217332c94996980331fd992
Justin Fagnani <justinfagnani@google.com>
2019-01-11 10:07:28 -0800

    Fix decorator types causing compiler errors for TypeScript users.

--- src/lib/decorators.ts
+++ src/lib/decorators.ts
@@ -21,6 +21,24 @@ export type Constructor<T> = {
   new (...args: any[]): T
 };
 
+// From the TC39 Decorators proposal
+interface ClassDescriptor {
+  kind: 'class';
+  elements: ClassElement[];
+  finisher?: <T>(clazz: Constructor<T>) => undefined | Constructor<T>;
+}
+
+// From the TC39 Decorators proposal
+interface ClassElement {
+  kind: 'field'|'method';
+  key: PropertyKey;
+  placement: 'static'|'prototype'|'own';
+  initializer?: Function;
+  extras?: ClassElement[];
+  finisher?: <T>(clazz: Constructor<T>) => undefined | Constructor<T>;
+  descriptor?: PropertyDescriptor;
+}
+
 const legacyCustomElement =
     (tagName: string, clazz: Constructor<HTMLElement>) => {
       window.customElements.define(tagName, clazz);
