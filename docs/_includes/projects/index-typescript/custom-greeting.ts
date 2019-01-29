dc02385fa53be2be7d7d79a9b3b10b92b90703fb
Justin Fagnani <justin@fagnani.com>
2019-01-29 15:41:25 -0500

    String is the default type
    Co-Authored-By: katejeffreys <katejeffreys@google.com>

--- docs/_includes/projects/index-typescript/custom-greeting.ts
+++ docs/_includes/projects/index-typescript/custom-greeting.ts
@@ -1,7 +1,7 @@
 import { LitElement, html, property } from 'lit-element';
 
 export class CustomGreeting extends LitElement {
-  @property({ type: String}) name = 'World';
+  @property() name = 'World';
   render() {
     return html`<p>Hello, ${this.name}!</p>`;
   }
