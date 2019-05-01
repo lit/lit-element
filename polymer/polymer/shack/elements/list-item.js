cd90fd63cd372971110805c1acce3d3f77494529
Alexander Marks <aomarks@google.com>
2019-05-01 14:39:32 -0700

    Polymer shack working, but uses lit

--- polymer/polymer/shack/elements/list-item.js
+++ polymer/polymer/shack/elements/list-item.js
@@ -9,9 +9,9 @@
  * rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-import {css, html, LitElement} from 'lit-element';
+import {html, PolymerElement} from '@polymer/polymer';
 
-export class ListItem extends LitElement {
+export class ListItem extends PolymerElement {
   static get properties() {
     return {
       title: {type: String},
@@ -19,35 +19,41 @@ export class ListItem extends LitElement {
     };
   }
 
-  static get styles() {
-    return css`
-      :host {
-        cursor: pointer;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-      }
+  static get template() {
+    return html`
+      <style>
+        :host {
+          cursor: pointer;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
 
-      .imagePlaceholder {
-        height: 125px;
-        width: 130px;
-        padding-top: 24px;
-        background-color: var(--list-item-placeholder-color, gray);
-      }
+        .imagePlaceholder {
+          height: 125px;
+          width: 130px;
+          padding-top: 24px;
+          background-color: var(--list-item-placeholder-color, gray);
+        }
 
-      .title {
-        color: #202020;
-        font-size: 13px;
-        font-weight: bold;
-        margin-top: 36px;
-        text-align: center;
-      }
+        .title {
+          color: #202020;
+          font-size: 13px;
+          font-weight: bold;
+          margin-top: 36px;
+          text-align: center;
+        }
 
-      .price {
-        color: #757575;
-        font-size: 13px;
-        margin-top: 4px;
-      }
+        .price {
+          color: #757575;
+          font-size: 13px;
+          margin-top: 4px;
+        }
+      </style>
+
+      <div class="imagePlaceholder"></div>
+      <span class="title">[[title]]</span>
+      <span class="price">$[[formatPrice()]]</span>
     `;
   }
 
@@ -57,12 +63,8 @@ export class ListItem extends LitElement {
     this.price = 0;
   }
 
-  render() {
-    return html`
-      <div class="imagePlaceholder"></div>
-      <span class="title">${this.title}</span>
-      <span class="price">\$${this.price.toFixed(2)}</span>
-    `;
+  formatPrice() {
+    return this.price.toFixed(2);
   }
 }
 
