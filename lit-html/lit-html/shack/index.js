1a8af9147c01533027f76af971273724e2415ddf
Alexander Marks <aomarks@google.com>
2019-05-01 17:17:19 -0700

    refactor lit-html app into app/cart/item modules

--- lit-html/lit-html/shack/index.js
+++ lit-html/lit-html/shack/index.js
@@ -9,12 +9,10 @@
  * rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-// import * as bench from '/bench.js';
-import {html, render} from '../node_modules/lit-html/lit-html.js';
+import {appData, renderApp} from './elements/shack-app.js';
 
-const data = {
-  page: 'mens_tshirts',
-  categories: {
+(async function() {
+  appData.categories = {
     'mens_outerwear': {
       title: 'Men\'s Outerwear',
     },
@@ -27,85 +25,17 @@ const data = {
     'ladies_tshirts': {
       title: 'Ladies T-Shirts',
     },
-  },
-  cart: [],
-};
+  };
 
-const renderPage = () => render(body(), document.body);
-
-const body = () => html`
-  <header id="pageHeader">${pageHeader()}</header>
-  <main id="categoryList">${categoryList()}</main>
-  <footer id="footer">${footer()}</footer>
-`;
-
-const pageHeader = () => html`
-  <h1 id="logo">SHACK</h1>
-  <div id="cartContainer">${cartContainer()}</div>
-  <nav id="categoryNav">${categoryNav()}</nav>
-`;
-
-const cartContainer = () => html`
-  <a id="cartButton">🛒</a>
-  <div id="cartBadge">${data.cart.length}</div>
-`;
-
-const categoryNav = () =>
-    Object.keys(data.categories)
-        .map(
-            (c) => html`<a ?active=${data.page === c} @click=${
-                (e) => selectCategory(c, e)}>${data.categories[c].title}</a>`);
-
-const selectCategory = (page, event) => {
-  event.preventDefault();
-  data.page = page;
-  renderPage();
-};
-
-const categoryList = () => html`
-  <div id="hero"></div>
-  <h2 id="categoryTitle">${data.categories[data.page].title}</h2>
-  <span id="numItems">(${data.categories[data.page].items.length} items)</span>
-
-  <div id="list">
-    ${data.categories[data.page].items.map(listItem)}
-  </div>
-`;
-
-const listItem = (item) => html`
-  <div class="listItem" @click=${(e) => clickItem(item, e)}>
-    <div class="imagePlaceholder"></div>
-    <span class="title">${item.title}</span>
-    <span class="price">\$${item.price.toFixed(2)}</span>
-  </div>
-`;
-
-const clickItem = (item, event) => {
-  event.preventDefault();
-  data.cart.push(item.title);
-  renderPage();
-};
-
-const footer = () => html`
-  <div id="demoNotice">DEMO ONLY</div>
-`;
-
-(async function() {
   const promises = [];
-  for (const c in data.categories) {
+  for (const c in appData.categories) {
     promises.push((async () => {
       const resp = await fetch(`./${c}.json`);
-      data.categories[c].items = await resp.json();
+      appData.categories[c].items = await resp.json();
+      appData.categories[c].slug = c;
     })());
   }
   await Promise.all(promises);
-  renderPage();
 
-  /**
-  setTimeout(() => {
-    bench.start();
-    renderPage();
-    bench.stop();
-  }, 100);
-  */
+  renderApp();
 })();
