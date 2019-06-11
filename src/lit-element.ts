f28501d349151d7c981e91cf0ff7225b0a662e0c
Alexander Marks <aomarks@google.com>
2019-06-11 12:44:14 -0700

    Update version tracker string

--- src/lit-element.ts
+++ src/lit-element.ts
@@ -32,7 +32,7 @@ declare global {
 // This line will be used in regexes to search for LitElement usage.
 // TODO(justinfagnani): inject version number at build time
 (window['litElementVersions'] || (window['litElementVersions'] = []))
-    .push('2.0.1');
+    .push('2.2.0');
 
 export interface CSSResultArray extends Array<CSSResult|CSSResultArray> {}
 
