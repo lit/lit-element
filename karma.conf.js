c8614adb567dfdd19c046219b40cbf5c844bb88b
Brendan Baldwin <brendan@usergenic.com>
2019-08-07 11:30:14 -0700

    Fixed the test command to run tests once and stop.  Fixed karma.conf.js to care only about test files.

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,9 @@
 module.exports = (config) => {
+  // Establish browsers to test by
   config.set({
     client: {runInParent: true, mocha: {ui: 'tdd'}},
     frameworks: ['mocha', 'chai', 'source-map-support'],
-    browsers: ['Chrome','Firefox','Safari'],
-    files: [
-      {pattern: 'test/**/*_test.js', type: 'module'},
-      {pattern: 'lit-*.js', included: false},
-      {pattern: 'lib/**/*.js.map', included: false},
-      {pattern: 'lib/**/*.js', included: false},
-      {pattern: 'test/**/*.js.map', included: false},
-      {pattern: 'test/**/*.js', included: false}
-    ]
+    browsers: ['Chrome', 'Firefox', 'Safari'],
+    files: [{pattern: 'test/**/*_test.js', type: 'module'}]
   })
 }
