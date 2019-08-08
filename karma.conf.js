e9e1582dd8de624e2c3e0fe591e05eb39bca9c6c
Brendan Baldwin <brendan@usergenic.com>
2019-08-08 15:04:25 -0700

    Reduce logging to LOG_INFO level.

--- karma.conf.js
+++ karma.conf.js
@@ -4,12 +4,16 @@ module.exports = (config) => {
     client: {runInParent: true, mocha: {ui: 'tdd'}},
     frameworks: ['mocha', 'chai', 'source-map-support'],
     files: [{pattern: 'test/**/*_test.js', type: 'module'}],
-    logLevel: config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+    reporters: ['spec']
   });
   if (localBrowsers.length > 0) {
+    console.log(
+        'KARMA_LOCAL_BROWSERS defined', process.env['KARMA_LOCAL_BROWSERS']);
     config.set({browsers: localBrowsers});
-  }
-  if (sauceBrowsers.length > 0) {
+  } else if (sauceBrowsers.length > 0) {
+    console.log(
+        'KARMA_SAUCE_BROWSERS defined', process.env['KARMA_SAUCE_BROWSERS']);
     config.set({
       sauceLabs: {
         testName: 'lit-element unit tests',
@@ -21,8 +25,6 @@ module.exports = (config) => {
       browserDisconnectTolerance: 3,
       browsers: Object.keys(customLaunchers),
       customLaunchers,
-      captureTimeout: 240000,
-      browserDisconnectTimeout: 240000,
       reporters: ['spec', 'saucelabs']
     });
   }
@@ -46,36 +48,30 @@ const sauceBrowsers =
      '')
         .split(',')
         .filter(Boolean)
-        .map((sauceBrowser) => {
+        .map((sauceBrowserSpec) => {
           const [browserName, afterBrowserName] =
-              sauceBrowser.split('@').filter(Boolean);
+              sauceBrowserSpec.split('@').filter(Boolean);
           const [version, afterVersion] =
               (afterBrowserName || '').split('/').filter(Boolean);
           const [platform, afterPlatform] =
               (afterVersion || '').split(':').filter(Boolean);
           const [deviceName, deviceOrientation] =
               (afterPlatform || '').split('|').filter(Boolean);
-          return {
-            base: 'SauceLabs',
-            browserName,
-            platform,
-            version,
-            deviceName,
-            deviceOrientation
-          };
+          const sauceBrowser =
+              {base: 'SauceLabs', browserName, platform, version};
+          if (deviceName) {
+            Object.assign(sauceBrowser, {deviceName, deviceOrientation});
+          }
+          return sauceBrowser;
         });
 
-const customLaunchers = sauceBrowsers.reduce(
-    (customLaunchers,
-     {base, browserName, platform, version, deviceName, deviceOrientation}) => {
-      const propertyName =
-          [browserName, platform, version, deviceName, deviceOrientation]
-              .join('_')
-              .toLowerCase()
-              .replace(/[^a-z0-9]+/g, '_')
-              .replace(/^_|_$/g, '');
-      customLaunchers[propertyName] =
-          {base, browserName, platform, version, deviceName, deviceOrientation};
-      return customLaunchers;
-    },
-    {});
+const customLaunchers = sauceBrowsers.reduce((customLaunchers, launcher) => {
+  const propertyName = Object.keys(launcher)
+                           .map((key) => launcher[key])
+                           .join('_')
+                           .toLowerCase()
+                           .replace(/[^a-z0-9]+/g, '_')
+                           .replace(/^_|_$/g, '');
+  customLaunchers[propertyName] = launcher;
+  return customLaunchers;
+}, {});
