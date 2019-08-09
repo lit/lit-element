71ee523c6efeeb1179af40660c74054a9b459fdc
Brendan Baldwin <brendan@usergenic.com>
2019-08-09 12:02:45 -0700

    Updated karma.conf.js to be more succinct and updated travis.yml to hopefully connect to sauce this time.

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,13 @@
 module.exports = (config) => {
-  // Establish browsers to test by
+  const {
+    KARMA_LOCAL_BROWSERS,
+    KARMA_SAUCE_BROWSERS,
+    SAUCE_USERNAME,
+    SAUCE_ACCESS_KEY,
+    TRAVIS_BUILD_NUMBER,
+    TRAVIS_JOB_NUMBER
+  } = process.env;
+
   config.set({
     client: {runInParent: true, mocha: {ui: 'tdd'}},
     frameworks: ['mocha', 'chai', 'source-map-support'],
@@ -7,19 +15,27 @@ module.exports = (config) => {
     logLevel: config.LOG_INFO,
     reporters: ['spec']
   });
+
+  const localBrowsers = (KARMA_LOCAL_BROWSERS || '').split(',').filter(Boolean);
+  const sauceBrowsers = SAUCE_USERNAME && SAUCE_ACCESS_KEY &&
+      parseSauceBrowsers(KARMA_SAUCE_BROWSERS);
+
   if (localBrowsers.length > 0) {
-    console.log(
-        'KARMA_LOCAL_BROWSERS defined', process.env['KARMA_LOCAL_BROWSERS']);
+    console.log('KARMA_LOCAL_BROWSERS defined', KARMA_LOCAL_BROWSERS);
     config.set({browsers: localBrowsers});
   } else if (sauceBrowsers.length > 0) {
-    console.log(
-        'KARMA_SAUCE_BROWSERS defined', process.env['KARMA_SAUCE_BROWSERS']);
+    console.log('KARMA_SAUCE_BROWSERS defined', KARMA_SAUCE_BROWSERS);
+    const customLaunchers = sauceBrowsers.reduce(
+        (customLaunchers, launcher) => Object.assign(
+            customLaunchers, {[keyFromValues(launcher)]: launcher}),
+        {});
     config.set({
       sauceLabs: {
         testName: 'lit-element unit tests',
-        startConnect: typeof travisJobNumber === 'undefined',
-        tunnelIdentifier: travisJobNumber,
-        idleTimeout: 300
+        startConnect: typeof TRAVIS_JOB_NUMBER === 'undefined',
+        tunnelIdentifier: TRAVIS_JOB_NUMBER,
+        idleTimeout: 300,
+        build: TRAVIS_BUILD_NUMBER
       },
       transports: ['polling'],
       browserDisconnectTolerance: 3,
@@ -30,48 +46,38 @@ module.exports = (config) => {
   }
 };
 
-const travisJobNumber = process.env['TRAVIS_JOB_NUMBER'];
-
-const localBrowsers =
-    (process.env['KARMA_LOCAL_BROWSERS'] || '').split(',').filter(Boolean);
+const keyFromValues = (obj) => Object.keys(obj)
+                                   .map((key) => obj[key].toString())
+                                   .join('_')
+                                   .toLowerCase()
+                                   .replace(/[^a-z0-9]+/g, '_')
+                                   .replace(/^_|_$/g, '');
 
 /**
- * Format of this ENV variable is:
- *     KARMA_SAUCE_BROWSERS="$platform/$browser@$version(:$deviceName|$deviceOrientation)?"
- * Example:
- *     KARMA_SAUCE_BROWSERS="windows 7/chrome@35,Android/Browser@4.4:Samsung
- * Galaxy S3 Emulator|portrait"
+ * Format of browsersList is:
+ * ```
+ * windows 7/chrome@35,Android/Browser@4.4:Samsung Galaxy S3
+ * Emulator|portrait
+ * ```
  */
-const sauceBrowsers =
-    (process.env['SAUCE_USERNAME'] && process.env['SAUCE_ACCESS_KEY'] &&
-         process.env['KARMA_SAUCE_BROWSERS'] ||
-     '')
-        .split(',')
-        .filter(Boolean)
-        .map((sauceBrowserSpec) => {
-          const [platform, afterPlatform] =
-              sauceBrowserSpec.split('/').filter(Boolean);
-          const [browserName, afterBrowserName] =
-              (afterPlatform || '').split('@').filter(Boolean);
-          const [version, afterVersion] =
-              (afterBrowserName || '').split(':').filter(Boolean);
-          const [deviceName, deviceOrientation] =
-              (afterVersion || '').split('|').filter(Boolean);
-          const sauceBrowser =
-              {base: 'SauceLabs', browserName, platform, version};
-          if (deviceName) {
-            Object.assign(sauceBrowser, {deviceName, deviceOrientation});
-          }
-          return sauceBrowser;
-        });
-
-const customLaunchers = sauceBrowsers.reduce((customLaunchers, launcher) => {
-  const propertyName = Object.keys(launcher)
-                           .map((key) => launcher[key])
-                           .join('_')
-                           .toLowerCase()
-                           .replace(/[^a-z0-9]+/g, '_')
-                           .replace(/^_|_$/g, '');
-  customLaunchers[propertyName] = launcher;
-  return customLaunchers;
-}, {});
+function parseSauceBrowsers(browsersList) {
+  return (browsersList || '')
+      .split(',')
+      .filter(Boolean)
+      .map((sauceBrowserSpec) => {
+        const [platform, afterPlatform] =
+            sauceBrowserSpec.split('/').filter(Boolean);
+        const [browserName, afterBrowserName] =
+            (afterPlatform || '').split('@').filter(Boolean);
+        const [version, afterVersion] =
+            (afterBrowserName || '').split(':').filter(Boolean);
+        const [deviceName, deviceOrientation] =
+            (afterVersion || '').split('|').filter(Boolean);
+        const sauceBrowser =
+            {base: 'SauceLabs', browserName, platform, version};
+        if (deviceName) {
+          Object.assign(sauceBrowser, {deviceName, deviceOrientation});
+        }
+        return sauceBrowser;
+      });
+}
