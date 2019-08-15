261e8b7a070e3af07d3a059f88f1f7761ec82e05
Brendan Baldwin <brendan@usergenic.com>
2019-08-15 15:45:09 -0700

    Simplified karma.proxy.js

--- karma.proxy.js
+++ karma.proxy.js
@@ -2,26 +2,10 @@ const Koa = require('koa');
 const mount = require('koa-mount');
 const staticFiles = require('koa-static');
 const {nodeResolve} = require('koa-node-resolve');
-const {
-  esmToAmd,
-  babelTransformEs2015,
-  babelTransformEs2016,
-  babelTransformEs2017,
-  babelTransformEs2018,
-  babelTransformModulesAmd
-} = require('koa-esm-to-amd');
+const {esmToAmd} = require('koa-esm-to-amd');
 
 module.exports = (karma) =>
-  new Koa()
-    .use(esmToAmd({
-          logLevel: 'debug'
-          // babelPlugins: [
-          //   ...babelTransformEs2015,
-          //   ...babelTransformEs2016,
-          //   ...babelTransformEs2017,
-          //   ...babelTransformEs2018,
-          //   ...babelTransformModulesAmd
-          // ]
-        }))
+    new Koa()
+        .use(esmToAmd())
         .use(mount('/base', new Koa().use(nodeResolve()).use(staticFiles('.'))))
         .use(karma);
