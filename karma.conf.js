707d4cb2f42d2c2725ce40ea3be5c17329192d6a
Brendan Baldwin <brendan@usergenic.com>
2019-08-15 16:26:13 -0700

    Make karma.conf.js not totally awful looking.

--- karma.conf.js
+++ karma.conf.js
@@ -1,65 +1,41 @@
 module.exports = (config) => {
-  const {
-    KARMA_LOCAL_BROWSERS,
-    KARMA_SAUCE_BROWSERS,
-    SAUCE_USERNAME,
-    SAUCE_ACCESS_KEY,
-    TRAVIS_BUILD_NUMBER,
-    TRAVIS_JOB_NUMBER
-  } = process.env;
-
   config.set({
+    browsers: [...localBrowsers, ...Object.keys(sauceBrowsers)],
     client: {runInParent: true, mocha: {ui: 'tdd'}},
-    // TODO(usergenic): 'source-map-support' framework's browser-source-map-support.js looks for `define` and then uses the requirejs module name feature.
-    // esm-to-amd proxy... not sure why yet.
     frameworks: ['mocha', 'chai', 'source-map-support'],
     files: [
       'node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js',
-      { pattern: 'test/lit-element_test.js', type: 'module' },
-      { pattern: 'test/lit-element_styling_test.js', type: 'module' },
-      { pattern: 'test/lib/decorators_test.js', type: 'module' },
-      { pattern: 'test/lib/updating-element_test.js', type: 'module'}
+      {pattern: 'test/lit-element_test.js', type: 'module'},
+      {pattern: 'test/lit-element_styling_test.js', type: 'module'},
+      {pattern: 'test/lib/decorators_test.js', type: 'module'},
+      {pattern: 'test/lib/updating-element_test.js', type: 'module'}
     ],
     logLevel: config.LOG_INFO,
     reporters: ['spec']
   });
 
-  const localBrowsers = (KARMA_LOCAL_BROWSERS || '').split(',').filter(Boolean);
-  const sauceBrowsers = SAUCE_USERNAME && SAUCE_ACCESS_KEY &&
-      parseSauceBrowsers(KARMA_SAUCE_BROWSERS);
-
-  if (localBrowsers && localBrowsers.length > 0) {
-    console.log('KARMA_LOCAL_BROWSERS defined', KARMA_LOCAL_BROWSERS);
-    config.set({browsers: localBrowsers});
-  } else if (sauceBrowsers && sauceBrowsers.length > 0) {
-    console.log('KARMA_SAUCE_BROWSERS defined', KARMA_SAUCE_BROWSERS);
-    const customLaunchers = sauceBrowsers.reduce(
-        (customLaunchers, launcher) => Object.assign(
-            customLaunchers, {[keyFromValues(launcher)]: launcher}),
-        {});
+  if (runTestsOnSauce) {
     config.set({
+      customLaunchers: sauceBrowsers,
       sauceLabs: {
         testName: 'lit-element unit tests',
-        startConnect: typeof TRAVIS_JOB_NUMBER === 'undefined',
-        tunnelIdentifier: TRAVIS_JOB_NUMBER,
+        startConnect: typeof process.env.TRAVIS_JOB_NUMBER === 'undefined',
+        tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
         idleTimeout: 300,
-        build: TRAVIS_BUILD_NUMBER
+        build: process.env.TRAVIS_BUILD_NUMBER
       },
       transports: ['polling'],
       browserDisconnectTolerance: 3,
-      browsers: Object.keys(customLaunchers),
-      customLaunchers,
       reporters: ['spec', 'saucelabs']
     });
   }
 };
 
-const keyFromValues = (obj) => Object.keys(obj)
-                                   .map((key) => obj[key].toString())
-                                   .join('_')
-                                   .toLowerCase()
-                                   .replace(/[^a-z0-9]+/g, '_')
-                                   .replace(/^_|_$/g, '');
+const localBrowsers =
+    (process.env.KARMA_LOCAL_BROWSERS || '').split(',').filter(Boolean);
+const sauceBrowsers = parseBrowserSpecs(process.env.KARMA_SAUCE_BROWSERS || '');
+const runTestsOnSauce = process.env.SAUCE_USERNAME &&
+    process.env.SAUCE_ACCESS_KEY && Object.keys(sauceBrowsers).length > 0;
 
 /**
  * Format of browsersList is:
@@ -68,24 +44,20 @@ const keyFromValues = (obj) => Object.keys(obj)
  * Emulator|portrait
  * ```
  */
-function parseSauceBrowsers(browsersList) {
-  return (browsersList || '')
-      .split(',')
-      .filter(Boolean)
-      .map((sauceBrowserSpec) => {
-        const [platform, afterPlatform] =
-            sauceBrowserSpec.split('/').filter(Boolean);
-        const [browserName, afterBrowserName] =
-            (afterPlatform || '').split('@').filter(Boolean);
-        const [version, afterVersion] =
-            (afterBrowserName || '').split(':').filter(Boolean);
-        const [deviceName, deviceOrientation] =
-            (afterVersion || '').split('|').filter(Boolean);
-        const sauceBrowser =
-            {base: 'SauceLabs', browserName, platform, version};
-        if (deviceName) {
-          Object.assign(sauceBrowser, {deviceName, deviceOrientation});
-        }
-        return sauceBrowser;
-      });
+function parseBrowserSpecs(browsersList) {
+  return browsersList.split(',').filter(Boolean).reduce((browsers, spec) => {
+    const match =
+        spec.match(/([^/]+)(?:\/([^@]+))?(?:@([^:]+))?(?:\:(.+)\|(.+))?/);
+    if (match) {
+      browsers[spec.replace(/[^A-Za-z0-9]+/g, '_')] = {
+        base: 'SauceLabs',
+        platform: match[1],
+        browserName: match[2],
+        version: match[3],
+        deviceName: match[4],
+        deviceOrientation: match[5]
+      };
+    }
+    return browsers;
+  }, {});
 }
