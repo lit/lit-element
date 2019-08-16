a22a9c2c9ffdb564196c01aa4e13c80dff0e2fc6
Brendan Baldwin <brendan@usergenic.com>
2019-08-15 17:34:06 -0700

    Up-to-dated koa-esm-transform (new package name)

--- karma.proxy.js
+++ karma.proxy.js
@@ -15,10 +15,10 @@ const Koa = require('koa');
 const mount = require('koa-mount');
 const staticFiles = require('koa-static');
 const {nodeResolve} = require('koa-node-resolve');
-const {esmToAmd} = require('koa-esm-to-amd');
+const {esmTransform} = require('koa-esm-transform');
 
 module.exports = (karma) =>
     new Koa()
-        .use(esmToAmd())
+        .use(esmTransform())
         .use(mount('/base', new Koa().use(nodeResolve()).use(staticFiles('.'))))
         .use(karma);
