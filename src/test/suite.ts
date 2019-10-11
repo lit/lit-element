8ffe57ffcbec5e0d48c6711b561487a3611cb4d7
Brendan Baldwin <brendan@usergenic.com>
2019-10-11 16:44:29 -0700

    Updated src/test/suite.ts because no const in js strict mode thanks, safari 9

--- src/test/suite.ts
+++ src/test/suite.ts
@@ -12,34 +12,36 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
-// @ts-ignore This will be present when this is run as JavaScript in the
-// browser.
-const suiteChild = window.suiteChild;
+(() => {
+  // @ts-ignore This will be present when this is run as JavaScript in the
+  // browser.
+  var suiteChild = window.suiteChild;
 
-suiteChild('Default Polyfills', '/base/test/runner.html');
-suiteChild('ShadyDOM Polyfill', '/base/test/runner.html?wc-shadydom=true');
-suiteChild(
-    'CustomElements Polyfill',
-    '/base/test/runner.html?wc-shadydom=true&wc-ce=true');
-suiteChild(
-    'Shim CSS Properties Polyfill',
-    '/base/test/runner.html?wc-shadydom=true&wc-ce=true&wc-shimcssproperties=true');
+  suiteChild('Default Polyfills', '/base/test/runner.html');
+  suiteChild('ShadyDOM Polyfill', '/base/test/runner.html?wc-shadydom=true');
+  suiteChild(
+      'CustomElements Polyfill',
+      '/base/test/runner.html?wc-shadydom=true&wc-ce=true');
+  suiteChild(
+      'Shim CSS Properties Polyfill',
+      '/base/test/runner.html?wc-shadydom=true&wc-ce=true&wc-shimcssproperties=true');
 
-// Currently Babel decorator compilation does not work on some older
-// browsers due to use of some newish features; detect these and don't
-// run babel-compiled tests on those browsers
-const fnNameDesc = Object.getOwnPropertyDescriptor(function() {}, 'name');
-const testBabel = fnNameDesc && fnNameDesc.configurable && Array.prototype.find;
+  // Currently Babel decorator compilation does not work on some older
+  // browsers due to use of some newish features; detect these and don't
+  // run babel-compiled tests on those browsers
+  var fnNameDesc = Object.getOwnPropertyDescriptor(function() {}, 'name');
+  var testBabel = fnNameDesc && fnNameDesc.configurable && Array.prototype.find;
 
-if (testBabel) {
-  suiteChild('Default Polyfills (Babel)', '/base/test/runner-babel.html');
-  suiteChild(
-      'ShadyDOM Polyfill (Babel)',
-      '/base/test/runner-babel.html?wc-shadydom=true');
-  suiteChild(
-      'CustomElements Polyfill (Babel)',
-      '/base/test/runner-babel.html?wc-shadydom=true&wc-ce=true');
-  suiteChild(
-      'Shim CSS Properties Polyfill (Babel)',
-      '/base/test/runner-babel.html?wc-shadydom=true&wc-ce=true&wc-shimcssproperties=true');
-}
+  if (testBabel) {
+    suiteChild('Default Polyfills (Babel)', '/base/test/runner-babel.html');
+    suiteChild(
+        'ShadyDOM Polyfill (Babel)',
+        '/base/test/runner-babel.html?wc-shadydom=true');
+    suiteChild(
+        'CustomElements Polyfill (Babel)',
+        '/base/test/runner-babel.html?wc-shadydom=true&wc-ce=true');
+    suiteChild(
+        'Shim CSS Properties Polyfill (Babel)',
+        '/base/test/runner-babel.html?wc-shadydom=true&wc-ce=true&wc-shimcssproperties=true');
+  }
+})();
