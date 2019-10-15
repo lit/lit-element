c27f556ee6dfab5226f890710cc208a02186e97a
Brendan Baldwin <brendan@usergenic.com>
2019-10-15 00:35:36 -0700

    Change reporter to summary.:

--- karma.conf.js
+++ karma.conf.js
@@ -89,7 +89,7 @@ const runTestsOnSauce = process.env.SAUCE_USERNAME &&
         .some((name) => customLaunchers[name].base === 'SauceLabs');
 
 const reporters = [
-  'spec',
+  'summary',
   runTestsOnBrowserStack && 'BrowserStack',
   runTestsOnSauce && 'saucelabs'
 ].filter(Boolean);
