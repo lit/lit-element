e7c749c36a625795526079390814b875e7b783bf
Justin Fagnani <justinfagnani@google.com>
2021-03-10 12:40:41 -0800

    Add getUpdateComplete() for forward-compat with LitElement 3.0

--- src/lib/updating-element.ts
+++ src/lib/updating-element.ts
@@ -832,8 +832,30 @@ export abstract class UpdatingElement extends HTMLElement {
    *       await this._myChild.updateComplete;
    *     }
    *   }
+   * @deprecated Override `getUpdateComplete()` instead for forward
+   *     compatibility with `lit-element` 3.0 / `@lit/reactive-element`.
    */
   protected _getUpdateComplete() {
+    return this.getUpdateComplete();
+  }
+
+  /**
+   * Override point for the `updateComplete` promise.
+   *
+   * It is not safe to override the `updateComplete` getter directly due to a
+   * limitation in TypeScript which means it is not possible to call a
+   * superclass getter (e.g. `super.updateComplete.then(...)`) when the target
+   * language is ES5 (https://github.com/microsoft/TypeScript/issues/338).
+   * This method should be overridden instead. For example:
+   *
+   *   class MyElement extends LitElement {
+   *     async getUpdateComplete() {
+   *       await super.getUpdateComplete();
+   *       await this._myChild.updateComplete;
+   *     }
+   *   }
+   */
+  protected getUpdateComplete() {
     return this._updatePromise;
   }
 
